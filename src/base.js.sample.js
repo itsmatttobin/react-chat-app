e9cf637662a9e2531885866ecbb2d191c1c8d2d9
Matt Tobin <matt@matttobin.co.uk>
2017-08-25 13:22:03 +0100

    Remove database connection

--- src/base.js.sample.js
+++ src/base.js.sample.js
@@ -2,9 +2,9 @@ import Rebase from 're-base';
 import firebase from 'firebase';
 
 const app = firebase.initializeApp({
-	apiKey: "AIzaSyDoIDE1xJ5EoiVhDxi42VtBW5FUb9kctmU",
-	authDomain: "react-chat-app-c03ce.firebaseapp.com",
-	databaseURL: "https://react-chat-app-c03ce.firebaseio.com"
+	apiKey: "",
+	authDomain: "",
+	databaseURL: ""
 });
 
 const base = Rebase.createClass(app.database());
